5c4fa7aeacf9a2e821f39855ca2881da22379b56
star120120 <49928596+star120120@users.noreply.github.com>
2026-07-19 18:55:56 +0800

    Add historical map name aliases for Iron Crown V3

---
+++ public/iron-crown-1936-v3/aliases.js
@@ -0,0 +1,7 @@
+'use strict';
+COUNTRY_DATA.GER.english.push('German Empire');
+COUNTRY_DATA.UK.english.push('British Empire');
+COUNTRY_DATA.ESP.english.push('Spanish State');
+COUNTRY_DATA.USSR.english.push('Russian Empire','Russian Federation');
+COUNTRY_DATA.CHE.english.push('Swiss Confederation');
+COUNTRY_DATA.DZG.english.push('Germany-Danzig');
